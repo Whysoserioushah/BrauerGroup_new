eaf3cf0b81648f6765c00dbddc20a151be8830cc
zjj <j.jj@jj.j>
2024-07-06 14:59:33 +0100

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -13,3 +13,9 @@ require "leanprover-community" / "mathlib"
 @[default_target]
 lean_lib «BrauerGroup» where
   -- add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
