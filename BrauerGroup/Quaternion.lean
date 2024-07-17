735c0085d1d94f7177237b4fff6eb26f6869a85a
benjzyq <123026302+benjzyq@users.noreply.github.com>
2024-07-17 14:59:43 +0100

    Update Quaternion.lean

--- BrauerGroup/Quaternion.lean
+++ BrauerGroup/Quaternion.lean
@@ -49,10 +49,58 @@ instance Gen_Quat_is_CSA [NeZero a] [NeZero b] : IsCentralSimple ℚ (ℍ[ℚ, a
     exact ⟨α, rfl⟩
   is_simple := Quat.quat_isSimple a b (NeZero.ne' a).symm (NeZero.ne' b).symm
 
+def Quat_to_tensor [NeZero a] [NeZero b] : ℍ[ℂ, a, b] →ₐ[ℂ] ℂ ⊗[ℚ] ℍ[ℚ, a, b] :=
+  QuaternionAlgebra.lift
+  {
+    i := (1 : ℂ) ⊗ₜ ⟨0,1,0,0⟩,
+    j := (1 : ℂ) ⊗ₜ ⟨0,0,1,0⟩,
+    k := (1 : ℂ) ⊗ₜ ⟨0,0,0,1⟩,
+    i_mul_i := by
+      simp only [Algebra.TensorProduct.tmul_mul_tmul, mul_one,
+        QuaternionAlgebra.mk_mul_mk, mul_zero, zero_add, add_zero, sub_zero, sub_self,
+        Algebra.TensorProduct.one_def]
+      rw [show ⟨a, 0, 0, 0⟩ = a • (1 : ℍ[ℚ, a, b]) by ext <;> simp]
+      rw [tmul_smul]; congr
+    j_mul_j := by
+      simp only [Algebra.TensorProduct.tmul_mul_tmul, mul_one,
+        QuaternionAlgebra.mk_mul_mk, mul_zero, zero_add, add_zero, sub_zero, sub_self,
+        Algebra.TensorProduct.one_def]
+      rw [show ⟨b, 0, 0, 0⟩ = b • (1 : ℍ[ℚ, a, b]) by ext <;> simp]
+      rw [tmul_smul]; congr
+    i_mul_j := by
+      simp only [Algebra.TensorProduct.tmul_mul_tmul, mul_one,
+        QuaternionAlgebra.mk_mul_mk, mul_zero, zero_add, add_zero, sub_zero, sub_self,
+        Algebra.TensorProduct.one_def]
+    j_mul_i := by
+      simp only [Algebra.TensorProduct.tmul_mul_tmul, mul_one, QuaternionAlgebra.mk_mul_mk,
+        mul_zero, add_zero, sub_self, zero_sub]
+      rw[show (⟨0, 0, 0, -1⟩:ℍ[ℚ, a, b]) = -1 • ⟨0, 0, 0, 1⟩ by ext <;> simp]
+      rw [tmul_smul, neg_one_zsmul]
+  }
 /-- prove 1 ⊗ 1, 1 ⊗ i, 1 ⊗ j, 1 ⊗ k is a basis of ℂ ⊗ ℍ. -/
+lemma Injective_Quat_to_tensor [NeZero a] [NeZero b]: Function.Injective (Quat_to_tensor a b) := by
+  change Function.Injective (Quat_to_tensor a b).toRingHom
+  have H := RingCon.IsSimpleOrder.iff_eq_zero_or_injective ℍ[ℂ , a, b] |>.1 $
+    Quat.quat_isSimple (a:ℂ) (b:ℂ) (by aesop) (by aesop)
+  specialize H (Quat_to_tensor a b).toRingHom
+  refine H.resolve_left fun rid => ?_
+  rw [eq_top_iff, RingCon.le_iff] at rid
+  specialize @rid 1 ⟨⟩
+  simp only [AlgHom.toRingHom_eq_coe, SetLike.mem_coe, RingCon.mem_ker, _root_.map_one,
+    one_ne_zero] at rid
+    
+lemma Surjective_Quat_to_tensor [NeZero a] [NeZero b]: Function.Surjective (Quat_to_tensor a b) := by
+  change Function.Surjective (Quat_to_tensor a b).toLinearMap
+  rw [← LinearMap.range_eq_top]
+  have eq := (Quat_to_tensor a b).toLinearMap.finrank_range_add_finrank_ker
+  rw [QuaternionAlgebra.finrank_eq_four, LinearMap.ker_eq_bot.2 (Injective_Quat_to_tensor a b), finrank_bot, add_zero] at eq
+  apply Submodule.eq_top_of_finrank_eq
+  · rw [eq]; symm; exact (finrank_four ℍ[ℚ,a,b] (QuaternionAlgebra.finrank_eq_four a b))
+
 theorem complex_tensor_eqv [NeZero a] [NeZero b] :
-    Nonempty (ℂ ⊗[ℚ] ℍ[ℚ, a, b] ≃ₐ[ℂ] ℍ[ℂ, a, b]) := by
-  sorry
+    Nonempty (ℍ[ℂ, a, b] ≃ₐ[ℂ] ℂ ⊗[ℚ] ℍ[ℚ, a, b]) :=
+  ⟨AlgEquiv.ofBijective (Quat_to_tensor a b) ⟨Injective_Quat_to_tensor _ _, Surjective_Quat_to_tensor _ _⟩ ⟩
+
 
 /-- use exist non-trvial but norm-zero element 1 + (1/√a) i -/
 def complex_quat_eqv (c d : ℂ) [NeZero c] [NeZero d]: ℍ[ℂ, c, d] ≃ₐ[ℂ] Matrix (Fin 2) (Fin 2) ℂ :=
