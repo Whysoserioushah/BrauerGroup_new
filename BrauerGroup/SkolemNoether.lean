1d6feab429df89fe09ac13aefa49e9b426a3ccbd
Yunzhou Xie <yx3021@ic.ac.uk>
2024-07-30 00:16:52 +0100

    bim

--- BrauerGroup/SkolemNoether.lean
+++ BrauerGroup/SkolemNoether.lean
@@ -61,32 +61,131 @@ instance(K A B M : Type u)
     [IsSimpleModule A M] (f: B →ₐ[K] A) : Ring (B ⊗[K] (Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ := inferInstance
 
 attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
+@[simp]
+def smul1AddHom'  (K A B M : Type u)
+    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A][Ring B] [Algebra K B]
+    [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
+    [IsSimpleModule A M] (f: B →ₐ[K] A):
+    (module_inst K A B M f) → B →+ (Module.End A M)ᵐᵒᵖ →+ (module_inst K A B M f) :=
+  fun m ↦ {
+    toFun := fun b ↦ {
+      toFun := fun l ↦ (f b) • (l.unop m)
+      map_zero' := by simp only [unop_zero, LinearMap.zero_apply, smul_zero]
+      map_add' := fun l1 l2 ↦ by simp only [LinearMapClass.map_smul, ← smul_add]; rfl
+    }
+    map_zero' := by ext; simp
+    map_add' := fun b1 b2 ↦ by
+      ext l; simp only [map_add, AddEquiv.toAddMonoidHom_eq_coe,
+      AddMonoidHom.coe_comp, AddMonoidHom.coe_mk, ZeroHom.coe_mk, AddMonoidHom.coe_coe,
+      opAddEquiv_apply, Function.comp_apply, unop_op, AddMonoidHom.add_apply];
+      exact Module.add_smul (f b1) (f b2) (l m)
+  }
+
+attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
+def smul1AddHom (K A B M : Type u)
+    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A][Ring B] [Algebra K B]
+    [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
+    [IsSimpleModule A M] (f: B →ₐ[K] A):
+    (module_inst K A B M f) → (B ⊗[K] (Module.End A M)ᵐᵒᵖ) →+ (module_inst K A B M f) := fun m ↦
+  TensorProduct.liftAddHom (smul1AddHom' K A B M f m) fun k b l ↦ by
+    simp only [smul1AddHom', AddMonoidHom.coe_mk, ZeroHom.coe_mk, LinearMapClass.map_smul,
+      smul_assoc, unop_smul, LinearMap.smul_apply]
+    rw [smul_comm]; congr!
+    sorry
+
+attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
+set_option synthInstance.maxHeartbeats 40000 in
 def smul1 (K A B M : Type u)
     [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A][Ring B] [Algebra K B]
     [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
     [IsSimpleModule A M] (f: B →ₐ[K] A):
     (module_inst K A B M f) → (B ⊗[K] (Module.End A M)ᵐᵒᵖ) →ₗ[K] (module_inst K A B M f) :=
-  fun m ↦ TensorProduct.lift {
-    toFun := fun b ↦ {
-      toFun := fun l ↦ (f b) • (l.unop m)
-      map_add' := fun l1 l2 ↦ by simp only [LinearMapClass.map_smul, ← smul_add]; rfl
-      map_smul' := fun k l ↦ by
-        simp only [LinearMapClass.map_smul, RingHom.id_apply]
-        rw [smul_comm]; simp only [unop_smul, LinearMap.smul_apply]
-    }
-    map_add' := fun _ _ ↦ by
-      ext _ ; simp [add_smul]
+  fun m ↦ {
+    __ := smul1AddHom K A B M f m
     map_smul' := fun k b ↦ by
-      ext l
-      simp only [LinearMapClass.map_smul, smul_assoc, LinearMap.map_smul_of_tower, LinearMap.coe_mk,
-        AddHom.coe_mk, RingHom.id_apply, LinearMap.smul_apply]
-      congr; rw [inst_K_mod];
-      ext k m
-      change k • m = algebraMap _ _ k • m
-      simp only [algebraMap_smul]
+      simp only [smul1AddHom, smul1AddHom', ZeroHom.toFun_eq_coe, AddMonoidHom.toZeroHom_coe,
+        RingHom.id_apply]
+      induction b using TensorProduct.induction_on
+      · simp only [smul_zero, map_zero]
+      · rename_i b l
+        simp only [TensorProduct.smul_tmul', TensorProduct.liftAddHom_tmul, AddMonoidHom.coe_mk,
+          ZeroHom.coe_mk, LinearMapClass.map_smul, smul_assoc]
+        congr
+        ext k m
+        change k • m = algebraMap _ _ k • m
+        simp only [algebraMap_smul]
+      · rename_i bl1 bl2 hbl1 hbl2
+        simp only [smul_add, map_add, hbl1, hbl2]
   }
+  -- fun m ↦ TensorProduct.lift {
+  --   toFun := fun b ↦ {
+  --     toFun := fun l ↦ (f b) • (l.unop m)
+  --     map_add' := fun l1 l2 ↦ by simp only [LinearMapClass.map_smul, ← smul_add]; rfl
+  --     map_smul' := fun k l ↦ by
+  --       simp only [LinearMapClass.map_smul, RingHom.id_apply]
+  --       rw [smul_comm]; simp only [unop_smul, LinearMap.smul_apply]
+  --   }
+  --   map_add' := fun _ _ ↦ by
+  --     ext _ ; simp [add_smul]
+  --   map_smul' := fun k b ↦ by
+  --     ext l
+  --     simp only [LinearMapClass.map_smul, smul_assoc, LinearMap.map_smul_of_tower, LinearMap.coe_mk,
+  --       AddHom.coe_mk, RingHom.id_apply, LinearMap.smul_apply]
+  --     congr; rw [inst_K_mod];
+  --     ext k m
+  --     change k • m = algebraMap _ _ k • m
+  --     simp only [algebraMap_smul]
+  -- }
+
+lemma one_smul1 (K A B M : Type u)
+    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A]
+    [Ring B] [Algebra K B]
+    [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
+    [IsSimpleModule A M] (f: B →ₐ[K] A):
+    ∀(m : module_inst K A B M f), smul1 K A B M f m 1 = m := fun m ↦ by
+  simp only [smul1, smul1AddHom, smul1AddHom', ZeroHom.toFun_eq_coe, AddMonoidHom.toZeroHom_coe,
+    Algebra.TensorProduct.one_def, LinearMap.coe_mk, AddHom.coe_mk, TensorProduct.liftAddHom_tmul,
+    AddMonoidHom.coe_mk, ZeroHom.coe_mk, map_one, one_smul, unop_one, LinearMap.one_apply]
+
+set_option synthInstance.maxHeartbeats 40000 in
+attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
+lemma mul_smul1 (K A B M : Type u)
+    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A]
+    [Ring B] [Algebra K B] [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
+    [IsSimpleModule A M] (f: B →ₐ[K] A):  ∀ (x y : (B ⊗[K] (Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ)
+    (m : module_inst K A B M f),
+    smul1 K A B M f m (x * y).unop = smul1 K A B M f (smul1 K A B M f m y.unop) x.unop :=
+  fun x y m ↦ by
+  simp only [smul1, smul1AddHom, smul1AddHom', ZeroHom.toFun_eq_coe, AddMonoidHom.toZeroHom_coe,
+    unop_mul, LinearMap.coe_mk, AddHom.coe_mk]
+  cases' x with x
+  cases' y with y
+  simp only [unop_mul, unop_op]
+  induction x using TensorProduct.induction_on
+  · simp only [mul_zero, map_zero]
+  · rename_i b1 l1
+    induction y using TensorProduct.induction_on
+    · sorry
+    -- · simp only [zero_mul, map_zero]
+    --   simp only [map_zero, smul_zero, ZeroHom.toFun_eq_coe,
+    --     AddMonoidHom.toZeroHom_coe, LinearMap.coe_mk, AddHom.coe_mk, TensorProduct.liftAddHom_tmul,
+    --     AddMonoidHom.coe_mk, ZeroHom.coe_mk]
+    · rename_i b2 l2
+      simp only [Algebra.TensorProduct.tmul_mul_tmul, TensorProduct.liftAddHom_tmul,
+        AddMonoidHom.coe_mk, ZeroHom.coe_mk, map_mul, unop_mul, LinearMap.mul_apply,
+        LinearMapClass.map_smul]
+      cases' l1 with l1
+      cases' l2 with l2
+      simp only [unop_op]
+      -- rw [smul_comm]
+      simp only [smul_smul]
+      sorry
+    · sorry
+  · sorry
 
 attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
+set_option maxHeartbeats 1600000 in
+set_option synthInstance.maxHeartbeats 40000 in
 instance (K A B M : Type u)
     [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A]
     [Ring B] [Algebra K B]
@@ -94,11 +193,7 @@ instance (K A B M : Type u)
     [IsSimpleModule A M] (f: B →ₐ[K] A) :
     Module (B ⊗[K] (Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ (module_inst K A B M f) where
   smul := fun r m => smul1 K A B M f m r.unop
-  one_smul m := by
-    change smul1 K A B M f m 1 = m
-    rw [smul1, Algebra.TensorProduct.one_def]
-    simp only [TensorProduct.lift.tmul, LinearMap.coe_mk, AddHom.coe_mk, map_one, one_smul,
-      unop_one, LinearMap.one_apply]
+  one_smul := one_smul1 K A B M f
   mul_smul x y m := sorry
   smul_zero a := by
     change smul1 K A B M f 0 a.unop = 0
@@ -106,8 +201,11 @@ instance (K A B M : Type u)
     cases' a with a
     simp only [map_zero, smul_zero, unop_op]
     induction a using TensorProduct.induction_on
-    · sorry
-    · sorry
+    · simp only [map_zero]
+    · rename_i b l
+      simp only [smul1AddHom, smul1AddHom', map_zero, smul_zero, ZeroHom.toFun_eq_coe,
+        AddMonoidHom.toZeroHom_coe, LinearMap.coe_mk, AddHom.coe_mk, TensorProduct.liftAddHom_tmul,
+        AddMonoidHom.coe_mk, ZeroHom.coe_mk]
     · sorry
   smul_add := sorry
   add_smul := sorry
