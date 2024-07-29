f4b06e9b090bbb3b3e958f5fa1dd9922a32214a4
Yunzhou Xie <yx3021@ic.ac.uk>
2024-07-29 15:20:27 +0100

    bim's work

--- BrauerGroup/SkolemNoether.lean
+++ BrauerGroup/SkolemNoether.lean
@@ -61,31 +61,6 @@ instance(K A B M : Type u)
     [IsSimpleModule A M] (f: B →ₐ[K] A) : Ring (B ⊗[K] (Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ := inferInstance
 
 attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
-set_option synthInstance.maxHeartbeats 40000 in
-def iso (K A B M : Type u)
-    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A][Ring B] [Algebra K B]
-    [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
-    [IsSimpleModule A M] (f: B →ₐ[K] A):
-    (B ⊗[K] (Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ ≃ₐ[K] (Bᵐᵒᵖ ⊗[K] ((Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ) :=
-  Algebra.TensorProduct.opAlgEquiv K K _ _|>.symm
-
--- attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
--- set_option synthInstance.maxHeartbeats 40000 in
--- def smul1 (K A B M : Type u)
---     [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A][Ring B] [Algebra K B]
---     [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
---     [IsSimpleModule A M] (f: B →ₐ[K] A):
---     (module_inst K A B M f) → (Bᵐᵒᵖ ⊗[K] ((Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ) →ₗ[K] (module_inst K A B M f) :=
---   fun m ↦ TensorProduct.lift {
---     toFun := fun bmop ↦ {
---       toFun := fun l ↦ (f (bmop.unop)) • (l.unop.unop m)
---       map_add' := _
---       map_smul' := _
---     }
---     map_add' := _
---     map_smul' := _
---   }
-attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
 def smul1 (K A B M : Type u)
     [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A][Ring B] [Algebra K B]
     [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
@@ -106,24 +81,10 @@ def smul1 (K A B M : Type u)
       simp only [LinearMapClass.map_smul, smul_assoc, LinearMap.map_smul_of_tower, LinearMap.coe_mk,
         AddHom.coe_mk, RingHom.id_apply, LinearMap.smul_apply]
       congr; rw [inst_K_mod];
-      sorry
+      ext k m
+      change k • m = algebraMap _ _ k • m
+      simp only [algebraMap_smul]
   }
-  -- fun m ↦ TensorProduct.lift {
-  --   toFun := fun b ↦ {
-  --     toFun := fun l ↦ _
-  --     map_add' := fun l1 l2 ↦ by simp only [LinearMapClass.map_smul, ← smul_add]; rfl
-  --     map_smul' := fun k l ↦ by
-  --       simp only [LinearMapClass.map_smul, RingHom.id_apply]
-  --       rw [smul_comm]; simp only [unop_smul, LinearMap.smul_apply]
-  --       congr; sorry
-  --   }
-  --   map_add' := fun _ _ ↦ by
-  --     ext _ ; simp [add_smul]
-  --   map_smul' := fun k b ↦ by
-  --     ext l
-  --     simp only [LinearMapClass.map_smul, smul_assoc, LinearMap.map_smul_of_tower, LinearMap.coe_mk,
-  --       AddHom.coe_mk, RingHom.id_apply, LinearMap.smul_apply]
-  -- }
 
 attribute [-instance] MulOpposite.instAddCommMonoid MulOpposite.instModule in
 instance (K A B M : Type u)
@@ -132,13 +93,25 @@ instance (K A B M : Type u)
     [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
     [IsSimpleModule A M] (f: B →ₐ[K] A) :
     Module (B ⊗[K] (Module.End A M)ᵐᵒᵖ)ᵐᵒᵖ (module_inst K A B M f) where
-  smul := fun rationalsomething m => smul1 K A B M f m (MulOpposite.unop rationalsomething)
+  smul := fun r m => smul1 K A B M f m r.unop
   one_smul := sorry
   mul_smul x y m := sorry
   smul_zero := sorry
   smul_add := sorry
   add_smul := sorry
   zero_smul := sorry
+
+theorem tensor_is_simple (K A B M : Type u)
+    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A] [Ring B] [Algebra K B]
+    [IsSimpleOrder (RingCon B)][AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
+    [IsSimpleModule A M] (f: B →ₐ[K] A): IsSimpleOrder (RingCon (B ⊗[K] (Module.End A M)ᵐᵒᵖ)) := by
+
+  sorry
+
+variable (K A B M : Type u)
+    [Field K] [Ring A] [Algebra K A] [FiniteDimensional K A]
+    [Ring B] [Algebra K B] [AddCommGroup M] [Module K M] [Module A M] [IsScalarTower K A M]
+    [IsSimpleModule A M] (f g : B →ₐ[K] A)
 
 -- -- lemma SkolemNoether_aux (A : Type u) [Ring A] [Algebra K A]
 -- --   (M : Type u) [AddCommGroup M] [Module A M] [Module K M] [IsScalarTower K A M]
