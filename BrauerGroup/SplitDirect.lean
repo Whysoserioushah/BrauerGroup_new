6025281bfcdb54d0f278af9c4b2523415380f8cb
ahhwuhu <ljq_0209@tju.edu.cn>
2024-08-17 15:10:15 +0800

    BrauerGroup/SplitDirect.lean

--- BrauerGroup/SplitDirect.lean
+++ BrauerGroup/SplitDirect.lean
@@ -2,6 +2,7 @@ import Mathlib.LinearAlgebra.FiniteDimensional
 import Mathlib.FieldTheory.IsSepclosed
 import BrauerGroup.BrauerGroup
 import BrauerGroup.ExtendScalar
+import BrauerGroup.Quaternion
 
 namespace split_direct
 
@@ -88,6 +89,12 @@ def intermediateTensorEquiv' (L : IntermediateField k K) :
   invFun := (intermediateTensorEquiv k K A L).symm
   left_inv := (intermediateTensorEquiv k K A L).left_inv
   right_inv := (intermediateTensorEquiv k K A L).right_inv
+
+lemma mem_intermediateTensor_iff_mem_intermediateTensor'
+    {L : IntermediateField k K} {x : K ⊗[k] A} :
+    x ∈ intermediateTensor k K A L ↔ x ∈ intermediateTensor' k K A L := by
+  simp only [intermediateTensor, LinearMap.mem_range, intermediateTensor', LinearMap.coe_mk,
+    LinearMap.coe_toAddHom]
 
 /- midfield leq then tensor leq -/
 lemma intermediateTensor_mono {L1 L2 : IntermediateField k K} (h : L1 ≤ L2) :
@@ -224,7 +231,7 @@ variable (n : ℕ) [NeZero n] (k K A : Type u) [Field k] [Field K] [Algebra k K]
   (iso : K ⊗[k] A ≃ₐ[K] Matrix (Fin n) (Fin n) K)
   (ℒ : Set (IntermediateField k K))
   (l_direct : DirectedOn (fun x x_1 ↦ x ≤ x_1) ℒ)
-  (h : ⨆ (L ∈ ℒ), L = ⊤)
+  (h : ⨆ (L ∈ ℒ), L = ⊤) (h1 : Nonempty ℒ)
 
 lemma is_direct : DirectedOn (fun x x_1 ↦ x ≤ x_1)
     (Set.range fun (L : ℒ) ↦ intermediateTensor k K A L) := by
@@ -235,10 +242,8 @@ lemma is_direct : DirectedOn (fun x x_1 ↦ x ≤ x_1)
 
 lemma SetOfInterField_nonempty : (Set.range fun (L : IntermediateField k K) ↦
     intermediateTensor k K A L).Nonempty := by
-  sorry
-
-def qqq (ℒ : Set (IntermediateField k K)) : Set (Subfield K) := by
-  sorry
+  rw [← Mathlib.Tactic.PushNeg.ne_empty_eq_nonempty]
+  simp only [ne_eq, Set.range_eq_empty_iff, not_isEmpty_of_nonempty, not_false_eq_true]
 
 theorem tensor_union_eq :
     ⨆ (L : ℒ), (intermediateTensor k K A L) = ⊤ := by
@@ -248,39 +253,32 @@ theorem tensor_union_eq :
   |zero => simp only [Submodule.zero_mem]
   |tmul x a =>
     obtain h₀ := IntermediateField.mem_top (F := k) (E := K) (x := x)
-    have h1 : Nonempty ℒ := by
-      -- by_contra!
-      -- have : ⨆ L ∈ ℒ, L = ⊥ := by simp_all
-      -- rw [this] at h
-      sorry
     rw [← h] at h₀
     replace h₀ : x ∈ (⨆ (L : ℒ), L.1) := by
       convert h₀
       rw [iSup_subtype]
     change x ∈ ((iSup (fun i : ℒ => i.1) : IntermediateField k K): Set K) at h₀
-    rw [IntermediateField.coe_iSup_of_directed] at h₀
+    rw [IntermediateField.coe_iSup_of_directed (directedOn_iff_directed.1 l_direct)] at h₀
     · simp only [Set.iUnion_coe_set, Set.mem_iUnion, SetLike.mem_coe, exists_prop] at h₀
-
-      have h1 : ∃ L, L ∈ ℒ ∧ x ∈ L :=  h₀
-      rcases h1 with ⟨L, ⟨hL1, hL2⟩⟩
+      rcases h₀ with ⟨L, ⟨hL1, hL2⟩⟩
       have h1 : (x ⊗ₜ[k] a) ∈ intermediateTensor k K A L := by
-        sorry
+        rw [intermediateTensor]
+        simp only [LinearMap.mem_range]
+        use (⟨x, hL2⟩ : ↥L) ⊗ₜ[k] a
+        simp only [LinearMap.rTensor_tmul, AlgHom.toLinearMap_apply, IntermediateField.val_mk]
       have h2 : (Set.range fun (L : ℒ) ↦ intermediateTensor k K A L).Nonempty := by
         refine Set.nonempty_def.mpr ?_
         tauto
       refine Submodule.mem_sSup_of_directed h2 (is_direct k K A ℒ l_direct) |>.2 ?_
       tauto
-    ·
-      sorry
   |add x y hx hy =>
     exact AddMemClass.add_mem hx hy
 
 theorem extension_element_in (x : K ⊗[k] A):
     ∃ (F : ℒ), x ∈ intermediateTensor k K A F := by
   have mem : x ∈ (⊤ : Submodule k _) := ⟨⟩
-  rw [← tensor_union_eq k K A ℒ l_direct h] at mem
-  have h1 : ℒ.Nonempty := by
-    sorry
+  rw [← tensor_union_eq k K A ℒ l_direct h h1] at mem
+  rw [Set.nonempty_iff_ne_empty', Mathlib.Tactic.PushNeg.ne_empty_eq_nonempty] at h1
   have h2 : (Set.range fun (L : ℒ) ↦ intermediateTensor k K A L).Nonempty := by
     refine Set.nonempty_def.mpr ?_
     use intermediateTensor k K A h1.choose
@@ -294,11 +292,19 @@ theorem extension_element_in (x : K ⊗[k] A):
   tauto
 
 def subfieldOf (x : K ⊗[k] A) : IntermediateField k K :=
-  extension_element_in k K A ℒ l_direct h x|>.choose
+  extension_element_in k K A ℒ l_direct h h1 x|>.choose
 
-lemma subfieldOf_in (x : K ⊗[k] A) : (subfieldOf k K A ℒ l_direct h x) ∈ ℒ := by
+lemma subfieldOf_in (x : K ⊗[k] A) : (subfieldOf k K A ℒ l_direct h h1 x) ∈ ℒ := by
   rw [subfieldOf]
   simp only [Subtype.coe_prop]
+
+theorem mem_subfieldOf (x : K ⊗[k] A) : x ∈ intermediateTensor k K A
+    (subfieldOf k K A ℒ l_direct h h1 x) := (extension_element_in k K A ℒ l_direct h h1 x).choose_spec
+
+theorem mem_subfieldOf' (x : K ⊗[k] A) : x ∈ intermediateTensor' k K A
+    (subfieldOf k K A ℒ l_direct h h1 x) := by
+  rw [← mem_intermediateTensor_iff_mem_intermediateTensor']
+  exact mem_subfieldOf k K A ℒ l_direct h h1 x
 
 def ee : Basis (Fin n × Fin n) K (K ⊗[k] A) :=
   Basis.map (Matrix.stdBasis K _ _) iso.symm
@@ -313,14 +319,206 @@ lemma ee_apply (i : Fin n × Fin n) : iso (e i) = Matrix.stdBasis K (Fin n) (Fin
   erw [← this]
 
 lemma L_sup :
-    ∃ L, L ∈ ℒ ∧ (∀ (i : Fin n × Fin n), subfieldOf k K A ℒ l_direct h (e i) ≤ L) := by
-  sorry
+    ∃ L, L ∈ ℒ ∧ (∀ (i : Fin n × Fin n), subfieldOf k K A ℒ l_direct h h1 (e i) ≤ L) := by
+  use ⨆ (i : Fin n × Fin n), subfieldOf k K A ℒ l_direct h h1 (e i)
+  constructor
+  · suffices ∀ i, subfieldOf k K A ℒ l_direct h h1 (e i) ∈ ℒ by
+      sorry
+    intro i
+    exact subfieldOf_in k K A ℒ l_direct h h1 (e i)
+  · let f := fun i => subfieldOf k K A ℒ l_direct h h1 (e i)
+    exact le_iSup f
 
-def ℒℒ : IntermediateField k K := (L_sup n k K A iso ℒ l_direct h).choose
+def ℒℒ : IntermediateField k K := (L_sup n k K A iso ℒ l_direct h h1).choose
 
-local notation "ℒ₁" => ℒℒ n k K A iso ℒ l_direct h
+local notation "ℒ₁" => ℒℒ n k K A iso ℒ l_direct h h1
 
-def isoRestrict' : ℒ₁ ⊗[k] A ≃ₐ[ℒ₁] Matrix (Fin n) (Fin n) ℒ₁ := sorry
+def f (i : Fin n × Fin n) : subfieldOf k K A ℒ l_direct h h1 (e i) →ₐ[k] ℒ₁ :=
+  subfieldOf k K A ℒ l_direct h h1 (e i)|>.inclusion
+  (by exact (L_sup n k K A iso ℒ l_direct h h1).choose_spec.2 i)
+
+def e_hat' (i : Fin n × Fin n) : intermediateTensor' k K A ℒ₁ :=
+  ⟨e i, by
+    rw [← mem_intermediateTensor_iff_mem_intermediateTensor', ℒℒ]
+    exact intermediateTensor_mono k K A ((L_sup n k K A iso ℒ l_direct h h1).choose_spec.2 i)
+      $ mem_subfieldOf k K A ℒ l_direct h h1 (e i)⟩
+
+local notation "e^'" => e_hat' n k K A iso ℒ l_direct h h1
+
+theorem e_hat_linear_independent : LinearIndependent ℒ₁ e^' := by
+  rw [linearIndependent_iff']
+  intro s g h
+  have h' : ∑ i ∈ s, algebraMap ℒ₁ K (g i) • e i = 0 := by
+    apply_fun Submodule.subtype _ at h
+    simpa only [IntermediateField.algebraMap_apply, map_sum, map_smul, Submodule.coeSubtype,
+      map_zero] using h
+
+  have H := (linearIndependent_iff'.1 $ e |>.linearIndependent) s (algebraMap ℒ₁ K ∘ g) h'
+  intro i hi
+  simpa using H i hi
+
+-- shortcut instance search
+set_option synthInstance.maxHeartbeats 40000 in
+instance : Module ℒ₁ (ℒ₁ ⊗[k] A) := TensorProduct.leftModule
+
+theorem dim_ℒ_eq : FiniteDimensional.finrank ℒ₁ (intermediateTensor' k K A ℒ₁) = n^2 := by
+    -- have eq1 := dim_eq k K A |>.trans iso.toLinearEquiv.finrank_eq
+    -- simp only [FiniteDimensional.finrank_matrix, Fintype.card_fin] at eq1
+    -- rw [pow_two, ← eq1, dim_eq k ℒ A]
+    -- exact LinearEquiv.finrank_eq (intermediateTensorEquiv' k k_bar A ℒ)
+    sorry
+
+def e_hat : Basis (Fin n × Fin n) ℒ₁ (intermediateTensor' k K A ℒ₁) :=
+  basisOfLinearIndependentOfCardEqFinrank (e_hat_linear_independent n k K A iso ℒ l_direct h h1) $ by
+    simp only [Fintype.card_prod, Fintype.card_fin, dim_ℒ_eq, pow_two]
+
+
+local notation "e^" => e_hat n k K A iso ℒ l_direct h h1
+
+def isoRestrict0 : ℒ₁ ⊗[k] A ≃ₗ[ℒ₁] Matrix (Fin n) (Fin n) ℒ₁ :=
+  (intermediateTensorEquiv' k K A ℒ₁).symm ≪≫ₗ
+  Basis.equiv (e^) (Matrix.stdBasis ℒ₁ (Fin n) (Fin n)) (Equiv.refl _)
+
+set_option synthInstance.maxHeartbeats 50000 in
+instance : SMulCommClass k ℒ₁ ℒ₁ := inferInstance
+-- instance : Algebra ℒ (ℒ ⊗[k] A) := Algebra.TensorProduct.leftAlgebra
+
+def inclusion : ℒ₁ ⊗[k] A →ₐ[ℒ₁] K ⊗[k] A :=
+  Algebra.TensorProduct.map (Algebra.ofId ℒ₁ K) (AlgHom.id k A)
+
+def inclusion' : Matrix (Fin n) (Fin n) ℒ₁ →ₐ[ℒ₁] Matrix (Fin n) (Fin n) K :=
+  AlgHom.mapMatrix (Algebra.ofId ℒ₁ _)
+
+lemma inclusion'_injective : Function.Injective (inclusion' n k K A iso ℒ l_direct h h1) := by
+  intro x y h
+  ext i j
+  rw [← Matrix.ext_iff] at h
+  specialize h i j
+  simp only [inclusion', Algebra.ofId, AlgHom.mapMatrix_apply, AlgHom.coe_mk, Matrix.map_apply,
+    IntermediateField.algebraMap_apply, SetLike.coe_eq_coe] at h
+  rw [h]
+
+/--
+ℒ₁ ⊗_k A ------>  intermidateTensor
+  |              /
+  | inclusion  /
+  v          /
+K ⊗_k A  <-
+-/
+lemma comm_triangle :
+    (intermediateTensor' k K A ℒ₁).subtype ∘ₗ
+    (intermediateTensorEquiv' k K A ℒ₁).symm.toLinearMap =
+    (inclusion n k K A iso ℒ l_direct h h1).toLinearMap := by
+  ext a
+  simp only [AlgebraTensorModule.curry_apply, curry_apply, LinearMap.coe_restrictScalars,
+    LinearMap.coe_comp, Submodule.coeSubtype, LinearEquiv.coe_coe, Function.comp_apply, inclusion,
+    AlgHom.toLinearMap_apply, Algebra.TensorProduct.map_tmul, _root_.map_one, AlgHom.coe_id, id_eq]
+  simp only [intermediateTensorEquiv', intermediateTensorEquiv, LinearEquiv.symm_symm,
+    LinearEquiv.coe_symm_mk]
+  rfl
+
+/--
+intermidateTensor ----> M_n(ℒ)
+  | val                 | inclusion'
+  v                     v
+k⁻ ⊗_k A ----------> M_n(k⁻)
+          iso
+-/
+lemma comm_square' :
+    iso.toLinearEquiv.toLinearMap.restrictScalars ℒ₁ ∘ₗ
+    (intermediateTensor' k K A ℒ₁).subtype =
+    (inclusion' n k K A iso ℒ l_direct h h1).toLinearMap ∘ₗ
+    Basis.equiv (e^) (Matrix.stdBasis ℒ₁ (Fin n) (Fin n)) (Equiv.refl _) := by
+  apply Basis.ext (e^)
+  intro i
+  conv_lhs => simp only [AlgEquiv.toLinearEquiv_toLinearMap, e_hat,
+    basisOfLinearIndependentOfCardEqFinrank,
+    Basis.coe_mk, e_hat', LinearMap.coe_comp, LinearMap.coe_restrictScalars, Submodule.coeSubtype,
+    Function.comp_apply, AlgEquiv.toLinearMap_apply, LinearEquiv.coe_coe, AlgHom.toLinearMap_apply]
+  simp only [ee_apply, LinearMap.coe_comp, LinearEquiv.coe_coe, Function.comp_apply,
+    Basis.equiv_apply, Equiv.refl_apply, AlgHom.toLinearMap_apply]
+  ext a b
+  simp only [inclusion', AlgHom.mapMatrix_apply, Matrix.map_apply]
+  simp only [Algebra.ofId, AlgHom.coe_mk, IntermediateField.algebraMap_apply]
+  rw [Matrix.stdBasis_eq_stdBasisMatrix]
+  erw [Matrix.stdBasis_eq_stdBasisMatrix]
+  simp only [Matrix.stdBasisMatrix]
+  aesop
+
+/--
+     isoRestrict
+  ℒ ⊗_k A -----> M_n(ℒ)
+    | inclusion    | inclusion'
+    v              v
+  k⁻ ⊗_k A -----> M_n(k⁻)
+            iso
+-/
+lemma comm_square :
+    (inclusion' n k K A iso ℒ l_direct h h1).toLinearMap ∘ₗ
+    (isoRestrict0 n k K A iso ℒ l_direct h h1).toLinearMap  =
+    iso.toLinearEquiv.toLinearMap.restrictScalars ℒ₁ ∘ₗ
+    (inclusion n k K A iso ℒ l_direct h h1).toLinearMap := by
+  rw [← comm_triangle n k K A iso ℒ l_direct h h1, ← LinearMap.comp_assoc,
+    comm_square' n k K A iso ℒ l_direct h h1]
+  rfl
+
+lemma isoRestrict_map_one : isoRestrict0 n k K A iso ℒ l_direct h h1 1 = 1 := by
+  /-
+        isoRestrict
+  ℒ ⊗_k A -----> M_n(ℒ)
+    | inclusion    | inclusion'
+    v              v
+  k⁻ ⊗_k A -----> M_n(k⁻)
+            iso
+
+  Want to show isoRestrict 1 = 1
+  inclusion' ∘ isoRestrict = iso ∘ inclusion
+  inclusion' (isoRestrict 1) = iso (inclusion 1) = 1 = inclusion' 1
+  since inclusion' is injective, isoRestrict 1 = 1
+  -/
+  have eq := congr($(comm_square n k K A iso ℒ l_direct h h1) 1)
+  conv_rhs at eq =>
+    rw [LinearMap.comp_apply]
+    erw [(inclusion n k K A iso ℒ l_direct h h1).map_one, map_one iso]
+  refine inclusion'_injective n k K A iso ℒ l_direct h h1 (eq.trans ?_)
+  rw [_root_.map_one]
+
+lemma isoRestrict_map_mul (x y : ℒ₁ ⊗[k] A) :
+    isoRestrict0 n k K A iso ℒ l_direct h h1 (x * y) =
+    isoRestrict0 n k K A iso ℒ l_direct h h1 x * isoRestrict0 n k K A iso ℒ l_direct h h1 y := by
+  /-
+        isoRestrict
+  ℒ ⊗_k A -----> M_n(ℒ)
+    | inclusion    | inclusion'
+    v              v
+  k⁻ ⊗_k A -----> M_n(k⁻)
+            iso
+
+  Want to show isoRestrict (x * y) = isoRestrict x * isoRestrict y
+  inclusion' ∘ isoRestrict = iso ∘ inclusion
+  inclusion' (isoRestrict (x * y)) = iso (inclusion (x * y)) = iso (inclusion x) * iso (inclusion y)
+    = inclusion' (isoRestrict x) * inclusion' (isoRestrict y)
+    = inclusion' (isoRestrict x * isoRestrict y)
+  since inclusion' is injective, isoRestrict (x * y) = isoRestrict x * isoRestrict y
+
+  -/
+  have eq := congr($(comm_square n k K A iso ℒ l_direct h h1) (x * y))
+  conv_rhs at eq =>
+    rw [LinearMap.comp_apply]
+    erw [(inclusion n k K A iso ℒ l_direct h h1).map_mul, _root_.map_mul (f := iso)]
+  have eq₁ := congr($(comm_square n k K A iso ℒ l_direct h h1) x)
+  have eq₂ := congr($(comm_square n k K A iso ℒ l_direct h h1) y)
+  simp only [LinearMap.coe_comp, LinearEquiv.coe_coe, Function.comp_apply, AlgHom.toLinearMap_apply,
+    AlgEquiv.toLinearEquiv_toLinearMap, LinearMap.coe_restrictScalars,
+    AlgEquiv.toLinearMap_apply] at eq₁ eq₂
+  rw [← eq₁, ← eq₂, ← _root_.map_mul] at eq
+  refine inclusion'_injective n k K A iso ℒ l_direct h h1 (eq.trans ?_)
+  rw [_root_.map_mul]
+
+
+def isoRestrict' : ℒ₁ ⊗[k] A ≃ₐ[ℒ₁] Matrix (Fin n) (Fin n) ℒ₁ :=
+  AlgEquiv.ofLinearEquiv (isoRestrict0 n k K A iso ℒ l_direct h h1)
+    (isoRestrict_map_one n k K A iso ℒ l_direct h h1) (isoRestrict_map_mul n k K A iso ℒ l_direct h h1)
 
 variable [Ring A] [Algebra k A] [Algebra k K]
 
@@ -334,17 +532,15 @@ def isSplit (L : Type u) [Field L] [Algebra k L] : Prop :=
 
 lemma spilt_iff_left (A : CSA k) (𝓁 : Set (IntermediateField k K))
     (l_direct : DirectedOn (fun x x_1 ↦ x ≤ x_1) 𝓁)
-    (h : ⨆ (L ∈ 𝓁), L = K) :
+    (h : ⨆ (L ∈ 𝓁), L = ⊤) (h₀ : Nonempty 𝓁) :
     isSplit k A K → (∃ L ∈ 𝓁, isSplit k A L) := by
   rintro ⟨n, ⟨hn, hnL⟩⟩
   obtain hnL' := hnL.some; clear hnL
-  have h1 : ⨆ L ∈ 𝓁, L = ⊤ := by
-
-    sorry
-  use (L_sup n k K A hnL' 𝓁 l_direct h1).choose
+  use (L_sup n k K A hnL' 𝓁 l_direct h h₀).choose
   constructor
-  · exact (L_sup n k K A hnL' 𝓁 l_direct h1).choose_spec.1
+  · exact (L_sup n k K A hnL' 𝓁 l_direct h h₀).choose_spec.1
   · use n; use hn
+    rw [← neZero_iff] at hn
     obtain h1 := isoRestrict' n k K A hnL' 𝓁
     simp [ℒℒ] at h1
     tauto
@@ -359,9 +555,9 @@ lemma spilt_iff_right (A : CSA k) (𝓁 : Set (IntermediateField k K)):
 
 theorem spilt_iff (A : CSA k) (𝓁 : Set (IntermediateField k K))
     (l_direct : DirectedOn (fun x x_1 ↦ x ≤ x_1) 𝓁)
-    (h : ⨆ (L ∈ 𝓁), L = K) :
+    (h : ⨆ (L ∈ 𝓁), L = ⊤) (h₀ : Nonempty 𝓁) :
     isSplit k A K ↔ (∃ L ∈ 𝓁, isSplit k A L) := by
-  exact ⟨spilt_iff_left _ _ _ _ l_direct h, spilt_iff_right _ _ _ _⟩
+  exact ⟨spilt_iff_left _ _ _ _ l_direct h h₀, spilt_iff_right _ _ _ _⟩
 
 end
 
