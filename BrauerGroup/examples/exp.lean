244e178548712837c8fb79b9c7f443b6f2c9cb8e
Whysoserioushah <yx3021@ic.ac.uk>
2025-05-04 21:22:19 +0100

    fix

--- BrauerGroup/examples/exp.lean
+++ BrauerGroup/examples/exp.lean
@@ -6,6 +6,3 @@ theorem rearrange_digits_diff_divisible_by_9
   Nat.modEq_iff_dvd.1 <| Nat.modEq_nine_digits_sum m'|>.trans
     <| congrFun (congrArg HMod.hMod (List.Perm.sum_eq h_digits_perm)) 9 |>.trans
     <| (Nat.modEq_nine_digits_sum m).symm
-
-example {K : Type*} [Field K] (R : Type*) [Ring R] [Algebra K R] :
-  Matrix (Fin 1) (Fin 1) R ≃ₐ[K] R := by exact?
