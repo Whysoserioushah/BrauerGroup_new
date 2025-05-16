9ee74648d2400561e38b5622f7a9504bc9e472cf
Yaël Dillies <yael.dillies@gmail.com>
2025-05-16 12:59:13 +0000

    No need to require doc-gen anymore

--- lakefile.lean
+++ lakefile.lean
@@ -18,6 +18,3 @@ lean_lib «BrauerGroup» where
 
 require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
