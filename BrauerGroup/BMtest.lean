e74f22e40267f55f18c9def4d15a1cbfa530cc08
Whysoserioushah <yx3021@ic.ac.uk>
2025-07-11 14:48:34 +0100

    yael job

---
+++ BrauerGroup/BMtest.lean
@@ -0,0 +1,10 @@
+-- import Mathlib
+
+-- theorem no : False := (by decide +kernel : (open Classical in if Type = Type then 0 else 0) ^ 1 ≠ 0) (by simp)
+
+-- def FLT : Prop := ∀ x y z n : Nat, 0 < x → 0 < y → 0 < z → 2 < n → x ^ n + y ^ n ≠ z ^ n
+
+-- theorem proofFLT : FLT := no.elim
+-- theorem proofNotFLT : ¬FLT := no.elim
+
+-- #print axioms no
